3f49deb2a9591aca1bb1e6b96d2591ba3ea482ed
김태현 <th2102da@gmail.com>
2025-09-18 16:01:09 +0900

    과과제제완료

--- src/color_filter.py
+++ src/color_filter.py
@@ -7,6 +7,22 @@ import matplotlib.pyplot as plt
 class Filters:
     # TODO: Image kernels
     Kernels = {
+        "original": np.array(
+        [[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=np.float32),
+        "blur": np.array(
+        [[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32) / 9,
+        "gaussian blur": np.array(
+        [[1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.float32) / 16,
+        "sharpen": np.array(
+        [[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.float32),
+        "sobel (x)": np.array(
+        [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32),
+        "sobel (y)": np.array(
+        [[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32),
+        "edge detection": np.array(
+        [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32),
+        "emboss":np.array(
+        [[-2, -1, 0], [-1, 1, 1], [0, 1, 2]], dtype=np.float32)
     }
 
     def __init__(self, kernels=Kernels):
